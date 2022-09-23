0004882ea4e52d7f7b49e1b58fc2b77001714717
viry <vbatten10@hotmail.com>
2022-09-22 19:37:27 -0500

    Agregar un TODO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 //NgRx
 import { StoreModule } from '@ngrx/store';
@@ -19,6 +20,7 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     TodoModule,
     StoreModule.forRoot({todos: todoReducer}),
     StoreDevtoolsModule.instrument({ 
--- src/app/todos/todo-add/todo-add.component.ts
+++ src/app/todos/todo-add/todo-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import * as actions from '../todo.actions';
 
 @Component({
   selector: 'app-todo-add',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-add.component.css']
 })
 export class TodoAddComponent implements OnInit {
-
-  constructor() { }
+  txtInput: FormControl 
+  
+  constructor(private store: Store<AppState>) { 
+    this.txtInput = new FormControl('Hola', Validators.required);
+  }
 
   ngOnInit(): void {
+  }
+
+  agregar(){
+    if(this.txtInput.invalid){
+      return;
+    }
+
+    this.store.dispatch(
+      actions.crear({
+        texto: this.txtInput.value
+      })
+    );
+
+    this.txtInput.reset();
   }
 
 }
