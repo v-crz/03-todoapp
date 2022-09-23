6e4cc12ab5415d1cc766f84cb2383df6cf55c92a
viry <vbatten10@hotmail.com>
2022-09-22 19:17:38 -0500

    Definiendo el AppState de la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 //NgRx
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { todoReducer } from './todos/todo.reducer';
 
 import { AppComponent } from './app.component';
 import { TodoModule } from './todos/todo.module';
 import { FooterComponent } from './footer/footer.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,11 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     TodoModule,
-    StoreModule.forRoot({todos: todoReducer})
+    StoreModule.forRoot({todos: todoReducer}),
+    StoreDevtoolsModule.instrument({ 
+      maxAge: 25, 
+      logOnly: environment.production 
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -2,7 +2,9 @@ import { createReducer, on } from '@ngrx/store';
 import { Todo } from './models/todo.model';
 import { crear } from './todo.actions';
 
-export const estadoInicial: Todo[] = [];
+export const estadoInicial: Todo[] = [
+    new Todo('Salvar al mundo')
+];
 
 export const todoReducer = createReducer(estadoInicial, 
     on(crear, (state, {texto}) => [
