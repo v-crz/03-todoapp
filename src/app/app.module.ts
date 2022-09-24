bff1cd4ce4295fda9fd08a4803dc6172df130fa3
viry <vbatten10@hotmail.com>
2022-09-23 21:17:43 -0500

    Combinando 2 o más reducers en el AppState

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-import { todoReducer } from './todos/todo.reducer';
-
 import { AppComponent } from './app.component';
 import { TodoModule } from './todos/todo.module';
 import { FooterComponent } from './footer/footer.component';
 import { environment } from '../environments/environment';
+import { appReducers } from './app.reducer';
 
 @NgModule({
   declarations: [
@@ -22,7 +21,7 @@ import { environment } from '../environments/environment';
     BrowserModule,
     ReactiveFormsModule,
     TodoModule,
-    StoreModule.forRoot({todos: todoReducer}),
+    StoreModule.forRoot(appReducers),
     StoreDevtoolsModule.instrument({ 
       maxAge: 25, 
       logOnly: environment.production 
