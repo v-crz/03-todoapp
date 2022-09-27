3beac5f142a9eee6172f0d400fa39ce44c6e5e5c
viry <vbatten10@hotmail.com>
2022-09-26 19:27:38 -0500

    Aplicar el filtro visualmente

--- src/app/todos/todo-list/todo-list.component.ts
+++ src/app/todos/todo-list/todo-list.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 
 import { Todo } from '../models/todo.model';
 import { AppState } from 'src/app/app.reducer';
+import { filtrosValidos } from 'src/app/filtro/filtro.actions';
 
 @Component({
   selector: 'app-todo-list',
@@ -11,12 +12,17 @@ import { AppState } from 'src/app/app.reducer';
 })
 export class TodoListComponent implements OnInit {
   todos: Todo[] = [];
+  filtroActual: filtrosValidos = 'todos';
 
   constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
-    this.store.select('todos')
-      .subscribe(todos => this.todos = todos);
+    // this.store.select('todos')
+    //   .subscribe(todos => this.todos = todos);
+    this.store.subscribe( ({todos, filtro}) => {
+      this.todos = todos;
+      this.filtroActual = filtro;
+    });
   }
 
 }
